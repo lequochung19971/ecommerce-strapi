6727eb28c6ad7bfe31e44a42f8053c25adc78434
iamhung <43690592+lequochung19971@users.noreply.github.com>
2023-01-15 22:46:28 +0700

    feat: update packge

---
+++ config/plugins.ts
@@ -0,0 +1,14 @@
+export default ({ env }) => ({
+  // email: {
+  //   config: {
+  //     provider: "sendgrid",
+  //     providerOptions: {
+  //       apiKey: env("SENDGRID_API_KEY"),
+  //     },
+  //     settings: {
+  //       defaultFrom: "lequochung199712@gmail.com",
+  //       defaultReplyTo: "lequochung199712@gmail.com",
+  //     },
+  //   },
+  // },
+});
